dfa756d6250b833affe9417bee75415c14815b26
Edmilson Scovino <scovinowork@gmail.com>
2021-06-26 10:39:24 -0300

    Class CadastroUsuario

---
+++ savemoneyback_end/src/main/java/com/example/savemoneyback_end/model/CadastroUsuario.java
@@ -0,0 +1,108 @@
+package com.example.savemoneyback_end.model;
+
+public class CadastroUsuario {
+
+    private String nome;
+    private String sobrenome;
+    private int dataNascimento;
+    private int cpf;
+    private int telefone;
+    private int cep;
+    private String rua;
+    private int numero;
+    private String cidade;
+    private String bairro;
+    private int senha;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public int getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(int dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public int getCep() {
+        return cep;
+    }
+
+    public void setCep(int cep) {
+        this.cep = cep;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public int getSenha() {
+        return senha;
+    }
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
+    public boolean cadastroNovoUsuario() {
+
+    }
+}
