b45fbe8c906a2325757b5c0f46d67e4cb84a64e3
Edmilson Scovino <scovinowork@gmail.com>
2021-06-27 11:34:47 -0300

    Tive que tirar os breaks estao dando erro

--- savemoneyback_end/src/main/java/com/example/savemoneyback_end/model/CadastroUsuario.java
+++ savemoneyback_end/src/main/java/com/example/savemoneyback_end/model/CadastroUsuario.java
@@ -2,17 +2,17 @@ package com.example.savemoneyback_end.model;
 
 public class CadastroUsuario {
 
-    private String
+
     private int idade;
     private String tipoUsuario;
     private int dataNascimento;
     private int lucroEmpresa;
-    private
+    private String tipoEmpresaJuridica;
 
 
 
 
-    public void cadastroNovoUsuario() {
+    public String cadastroNovoUsuario() {
         switch(tipoUsuario) {
             case "pessoaFisica":
                     if (idade < 18){
@@ -20,7 +20,6 @@ public class CadastroUsuario {
                     }else{
                         return "Ok, maior de 18 anos";
                     }
-                break;
             case "pessoaJuridica":
                 switch(tipoEmpresaJuridica) {
                     case "autonomo":
@@ -29,37 +28,72 @@ public class CadastroUsuario {
                         }else{
                             return "Voce não se classifica";
                         }
-                        break;
                     case "mei":
                         if (lucroEmpresa <= 80000){
                             return "Voce se classfica como MEI";
                         }else{
                             return "Voce não se classifica";
                         }
-                        break;
                     case "empresarioIndividual":
                         if (lucroEmpresa <= 300000){
                             return "Voce se classfica como MEI";
                         }else{
                             return "Voce não se classifica";
                         }
-                        break;
+
                     case "sociedade":
                         if (lucroEmpresa <= 1000000){
                             return "Voce se classfica como MEI";
                         }else{
                             return "Voce não se classifica";
                         }
-                        break;
-                    default:
-                        // code block
+
                 }
-
-
                 break;
             default:
                 return "Não pode realizar o cadastro";
         }
 
+        return null;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public int getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(int dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public int getLucroEmpresa() {
+        return lucroEmpresa;
+    }
+
+    public void setLucroEmpresa(int lucroEmpresa) {
+        this.lucroEmpresa = lucroEmpresa;
+    }
+
+    public String getTipoEmpresaJuridica() {
+        return tipoEmpresaJuridica;
+    }
+
+    public void setTipoEmpresaJuridica(String tipoEmpresaJuridica) {
+        this.tipoEmpresaJuridica = tipoEmpresaJuridica;
     }
 }
