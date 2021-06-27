9bafb63b25c1861abb5d3247fcb59fcf00aa9977
Edmilson Scovino <scovinowork@gmail.com>
2021-06-27 11:21:20 -0300

    Função que tipo de Usuario;

--- savemoneyback_end/src/main/java/com/example/savemoneyback_end/model/CadastroUsuario.java
+++ savemoneyback_end/src/main/java/com/example/savemoneyback_end/model/CadastroUsuario.java
@@ -2,107 +2,64 @@ package com.example.savemoneyback_end.model;
 
 public class CadastroUsuario {
 
-    private String nome;
-    private String sobrenome;
+    private String
+    private int idade;
+    private String tipoUsuario;
     private int dataNascimento;
-    private int cpf;
-    private int telefone;
-    private int cep;
-    private String rua;
-    private int numero;
-    private String cidade;
-    private String bairro;
-    private int senha;
+    private int lucroEmpresa;
+    private
 
-    public String getNome() {
-        return nome;
-    }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 
-    public String getSobrenome() {
-        return sobrenome;
-    }
 
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
-    }
+    public void cadastroNovoUsuario() {
+        switch(tipoUsuario) {
+            case "pessoaFisica":
+                    if (idade < 18){
+                        return "Não pode criar uma conta";
+                    }else{
+                        return "Ok, maior de 18 anos";
+                    }
+                break;
+            case "pessoaJuridica":
+                switch(tipoEmpresaJuridica) {
+                    case "autonomo":
+                        if (lucroEmpresa <= 60000){
+                            return "Voce se classfica como Autonomo";
+                        }else{
+                            return "Voce não se classifica";
+                        }
+                        break;
+                    case "mei":
+                        if (lucroEmpresa <= 80000){
+                            return "Voce se classfica como MEI";
+                        }else{
+                            return "Voce não se classifica";
+                        }
+                        break;
+                    case "empresarioIndividual":
+                        if (lucroEmpresa <= 300000){
+                            return "Voce se classfica como MEI";
+                        }else{
+                            return "Voce não se classifica";
+                        }
+                        break;
+                    case "sociedade":
+                        if (lucroEmpresa <= 1000000){
+                            return "Voce se classfica como MEI";
+                        }else{
+                            return "Voce não se classifica";
+                        }
+                        break;
+                    default:
+                        // code block
+                }
 
-    public int getDataNascimento() {
-        return dataNascimento;
-    }
 
-    public void setDataNascimento(int dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
-
-    public int getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(int cpf) {
-        this.cpf = cpf;
-    }
-
-    public int getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(int telefone) {
-        this.telefone = telefone;
-    }
-
-    public int getCep() {
-        return cep;
-    }
-
-    public void setCep(int cep) {
-        this.cep = cep;
-    }
-
-    public String getRua() {
-        return rua;
-    }
-
-    public void setRua(String rua) {
-        this.rua = rua;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
-
-    public String getCidade() {
-        return cidade;
-    }
-
-    public void setCidade(String cidade) {
-        this.cidade = cidade;
-    }
-
-    public String getBairro() {
-        return bairro;
-    }
-
-    public void setBairro(String bairro) {
-        this.bairro = bairro;
-    }
-
-    public int getSenha() {
-        return senha;
-    }
-
-    public void setSenha(int senha) {
-        this.senha = senha;
-    }
-
-    public boolean cadastroNovoUsuario() {
+                break;
+            default:
+                return "Não pode realizar o cadastro";
+        }
 
     }
 }
