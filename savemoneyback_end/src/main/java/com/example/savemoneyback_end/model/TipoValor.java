0f8d3011520215c6facfce0c801a70f302eda624
mnshade <47904144+mnshade@users.noreply.github.com>
2021-06-30 19:56:38 -0300

    Create TipoValor.java

---
+++ savemoneyback_end/src/main/java/com/example/savemoneyback_end/model/TipoValor.java
@@ -0,0 +1,38 @@
+package com.example.savemoneyback_end.model;
+
+import com.example.savemoneyback_end.IReceita;
+
+public class TipoValor {
+
+    public String categoriaValor(IReceita valor_receita){
+
+        double valor = valor_receita.getValor_receita();
+
+        if(valor > 50){ //1
+            if (valor < 100) { //2
+                return "É um bom valor para começar!" ; //3
+            }else if (valor < 200) { //4
+                return "Devagar vamos longe."; //5
+            } else if (valor < 300) { //6
+                return "Acumulando e acumulando"; //7
+            } else if (valor < 400) { //8
+                return "Bora bora!"; //9
+            } else if (valor < 500) {  //10
+                return "Quase quinhentos, hein.";  //11
+            } else if (valor < 600) { //12
+                return "Boa, tá no foco!";  //13
+            } else if (valor < 700) { //14
+                return "Deixou de ir no mercado?"; //15
+            } else if (valor < 800) { //16
+                return "Grana da viagem, OK!"; //17
+            } else if (valor < 900) { //18
+                return "Virou economista?"; //19
+            } else if (valor == 1000) {  //20
+                return "Milzão? Tá bem na fita!"; //21
+            } else {
+                return "Nossa! Que baita grana."; //22
+            }
+        }
+        return "Nem precisa cadastrar, guarda no cofre."; //23
+    }//24
+}
